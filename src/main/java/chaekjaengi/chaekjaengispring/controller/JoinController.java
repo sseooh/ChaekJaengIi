5462845e6d83d201bb69a1e23a8a02e9111b9f09
unknown <leejieun0000@naver.com>
2023-02-10 17:14:59 +0900

    hello

--- src/main/java/chaekjaengi/chaekjaengispring/controller/JoinController.java
+++ src/main/java/chaekjaengi/chaekjaengispring/controller/JoinController.java
@@ -1,13 +1,37 @@
 package chaekjaengi.chaekjaengispring.controller;
 
+import chaekjaengi.chaekjaengispring.domain.Member;
+import chaekjaengi.chaekjaengispring.service.MemberService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 
 @Controller
 public class JoinController {
 
+    private final MemberService memberService;
+
+
+    @Autowired
+    public JoinController(MemberService memberService) {
+        this.memberService = memberService;
+    }
+
+
     @GetMapping("members/join")
     public String joinForm() {
         return "members/join";
+    }
+
+    @PostMapping("members/join")
+    public String create(MemberForm form){
+        Member member = new Member();
+        member.setId(form.getId());
+        member.setPwd(form.getPwd());
+
+        memberService.join(member);
+
+        return "home"; //??
     }
 }
---
+++ src/main/java/chaekjaengi/chaekjaengispring/controller/MemberForm.java
@@ -0,0 +1,24 @@
+package chaekjaengi.chaekjaengispring.controller;
+
+public class MemberForm {
+
+    private String id;
+
+    private String pwd;
+
+    public String getId(){
+        return id;
+    }
+
+    public void setId(String id){
+        this.id = id;
+    }
+
+    public String getPwd(){
+        return pwd;
+    }
+
+    public void setPwd(String pwd){
+        this.pwd = pwd;
+    }
+}
