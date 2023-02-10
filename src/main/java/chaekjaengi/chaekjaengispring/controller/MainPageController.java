4cc1aa8039bbaf188fa3f55edf3a8be268c8ecd8
sseooh <sseooh@gmail.com>
2023-02-10 17:17:13 +0900

    commit

--- src/main/java/chaekjaengi/chaekjaengispring/controller/MainPageController.java
+++ src/main/java/chaekjaengi/chaekjaengispring/controller/MainPageController.java
@@ -2,11 +2,12 @@ package chaekjaengi.chaekjaengispring.controller;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 
 @Controller
 public class MainPageController {
 
-    @GetMapping("mainPage")
+    @PostMapping("mainPage")
     public String MainPage() {
         return "mainPage";
     }
