a45064bb5a118ea536b60b473ac37c673f199e20
unknown <leejieun0000@naver.com>
2023-02-10 18:59:53 +0900

    change2

--- src/main/java/chaekjaengi/chaekjaengispring/repository/MemoryMemberRepository.java
+++ src/main/java/chaekjaengi/chaekjaengispring/repository/MemoryMemberRepository.java
@@ -10,26 +10,29 @@ import java.util.Optional;
 @Repository
 public class MemoryMemberRepository implements MemberRepository {
 
-    private static Map<String, String> store = new HashMap<>();
+    private static Map<String, Member> store = new HashMap<>();
 
     @Override
     public Member save(Member member) {
-        store.put(member.getId(), member.getPwd());
+        store.put(member.getId(), member);
         return member;
     }
 
     @Override
-    public Optional<Member> findById(String id) {
+    public Optional<String> findById(String id) {
         return store.keySet().stream()
-                .filter(member -> member.getId().equals(id))
+                .filter(member -> member.equals(id))
                 .findAny();
     }
 
     @Override
-    public Optional<Member> findByPwd(String pwd) {
+    public Optional<String> findByPwd(String pwd) {
         return store.values().stream()
-                .filter(member -> member.getPwd().equals(pwd))
+                .filter(member -> member.equals(pwd))
                 .findAny();
     }
 
+    public void clearStore(){
+        store.clear();
+    }
 }
--- src/test/java/chaekjaengi/chaekjaengispring/MemoryMemberRepositoryTest.java
+++ src/test/java/chaekjaengi/chaekjaengispring/MemoryMemberRepositoryTest.java
@@ -1,2 +1,55 @@
-package chaekjaengi.chaekjaengispring;public class MemoryMemberRepositoryTest {
+package chaekjaengi.chaekjaengispring;
+
+import chaekjaengi.chaekjaengispring.domain.Member;
+import chaekjaengi.chaekjaengispring.repository.MemoryMemberRepository;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.Test;
+
+import java.util.Optional;
+
+import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
+
+public class MemoryMemberRepositoryTest {
+
+    MemoryMemberRepository repository = new MemoryMemberRepository();
+
+    @AfterEach
+    public void afterEach(){
+        repository.clearStore();
+    }
+
+    @Test
+    public void save(){
+        //given
+        Member member= new Member();
+        member.setId("leejieun");
+        member.setPwd("12345");
+
+        //when
+        repository.save(member);
+
+        //then
+        Optional<String> result = repository.findById(member.getId());
+        Optional<String> result2 = repository.findByPwd(member.getPwd());
+        assertThat(member.getId()).isEqualTo(result);
+        // member.getId와 result의 값이 같으면 정상 실행 (save 실행 했을 때)
+        assertThat(member.getPwd()).isEqualTo(result2);
+    }
+
+    @Test
+    public void findById(){
+        Member member1 = new Member();
+        member1.setId("leejieun");
+        repository.save(member1);
+
+        Member member2 = new Member();
+        member2.setId("leejieun");
+        repository.save(member2);
+
+        String result = repository.findById("leejieun").get();
+        assertThat(result).isEqualTo(member1.getId());
+
+
+    }
+
 }
