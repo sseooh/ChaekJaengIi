8606864f11f7ca8c2023149343c7c221b0c6b5e3
unknown <leejieun0000@naver.com>
2023-03-10 16:29:57 +0900

    oh

--- src/main/java/chaekjaengi/chaekjaengispring/controller/ReviewController.java
+++ src/main/java/chaekjaengi/chaekjaengispring/controller/ReviewController.java
@@ -2,7 +2,6 @@ package chaekjaengi.chaekjaengispring.controller;
 
 import chaekjaengi.chaekjaengispring.domain.Member;
 import chaekjaengi.chaekjaengispring.domain.Review;
-import chaekjaengi.chaekjaengispring.repository.MemoryMemberRepository;
 import chaekjaengi.chaekjaengispring.repository.ReviewRepository;
 import chaekjaengi.chaekjaengispring.service.ReviewService;
 import chaekjaengi.chaekjaengispring.web.SessionConstants;
@@ -22,28 +21,20 @@ public class ReviewController {
     private final ReviewService reviewService;
     private final ReviewRepository reviewRepository;
 
-    private final MemoryMemberRepository memoryMemberRepository;
 
     @Autowired
-    public ReviewController(ReviewService reviewService, ReviewRepository reviewRepository, MemoryMemberRepository memoryMemberRepository) {
+    public ReviewController(ReviewService reviewService, ReviewRepository reviewRepository) {
         this.reviewService = reviewService;
         this.reviewRepository = reviewRepository;
-        this.memoryMemberRepository = memoryMemberRepository;
     }
 
 
     @RequestMapping(value = "/saveReview", method = RequestMethod.POST, produces = "application/html; charset=UTF-8")
     public String storeCheck(ReviewForm reviewForm, @SessionAttribute(name = "loginMember") Member member)throws Exception{
 
-        //HttpSession session = request.getSession();
-        //Member constant_member = (Member) session.getAttribute("loginMember").;
-        //String constant_id = constant_member.getId();
-
         Review review = new Review();
-        //책 제목이랑 아이디 받아오기
 
         review.setTitle("이지은");
-        //review.setId("leejieun");
         review.setId(member.getId());
         review.setName(reviewForm.getName());
         review.setContent(reviewForm.getContent());
